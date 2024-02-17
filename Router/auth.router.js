dd7e896660df3d6241d738e92f495bbb7bd0b475
Nav1306 <ce20b018@iittp.ac.in>
2024-02-18 00:13:27 +0530

    made controller

--- Router/auth.router.js
+++ Router/auth.router.js
@@ -1,20 +1,11 @@
 const express = require ("express");
-const jwt = require("jsonwebtoken");
-const userData = require("../db/users");
+
+const loginHandler = require("../controllers/authController")
 
 const loginRouter = express.Router();
 
 
 loginRouter.route("/")
-.post((req,res) => {
-    const {username,password} = req.body;
-    const isUserVerified = userData.users.some(user=>user.username === username && user.password === password);
-    if(isUserVerified){
-        const token = jwt.sign({id:username},process.env.SECRET_TOKEN);
-        res.json({username, token, message:"User Verified"})
-    }else {
-        res.status(401).json({message: "Invalid Credentials"})
-    }
-});
+.post((loginHandler));
 
 module.exports = loginRouter;
