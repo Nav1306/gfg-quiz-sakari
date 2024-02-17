cfb5f965892b32769ad0e7ece5b42eba11cb6c62
Nav1306 <ce20b018@iittp.ac.in>
2024-02-17 15:02:19 +0530

    first git commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/" , (req,res) => {
+    res.send("Hello Geeks !!");
+});
+
+app.listen(process.env.PORT || PORT , () => {
+    console.log("Server Started!!")
+})
