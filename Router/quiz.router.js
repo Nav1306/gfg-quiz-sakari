a8f8353bce8bad6a074a391f35bebb79fef38b25
Nav1306 <ce20b018@iittp.ac.in>
2024-02-18 12:37:03 +0530

    auth verify made

--- Router/quiz.router.js
+++ Router/quiz.router.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const quizzes = require("../db/quizzes");
 const quizRouter = express.Router();
+const {authVerify} = require("../controllers/authController")
 
 
-quizRouter.get("/" , (req,res) => {
-    res.json(quizzes.data);
+quizRouter.route("/")
+.get(authVerify,(req,res)=>{
+    res.json(quizzes.data)
 })
 
 module.exports = quizRouter;
--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,18 @@ const jwt = require("jsonwebtoken");
 const userData = require("../db/users");
 const {v4:uuid} = require("uuid");
 
+//creating a middleware to verify the token
+const authVerify = (req,res,next) => {
+    const token = req.headers.authorization;
+    try{
+        const decodedToken = jwt.verify(token,process.env.SECRET_TOKEN);
+        req.user = {userID:decodedToken.id};
+        return next();
+    }catch(err){
+      console.error(`error from server ${JSON.stringify(err)}`)
+    }
+}
+
 const loginHandler = (req, res) => {
   const { username, password } = req.body;
   const isUserVerified = userData.users.some(
@@ -20,7 +32,7 @@ const signupHandler = (req,res) => {
     const isUserPresent = userData.users.some((user) => {
       if(user.username === username){
         return true;
-      }
+      } 
     });
     if(isUserPresent){
       res.json("User Already Exists !!")
@@ -28,8 +40,9 @@ const signupHandler = (req,res) => {
     else {
       let newUser = {id:uuid(),username,password};
       userData.users = [...userData.users,newUser];
-      res.json({username,password,message:"User Added!"});
+      const token = jwt.sign({id:username},process.env.SECRET_TOKEN)
+      res.json({username,token,password,message:"User Added!"});
     }
 };
 
-module.exports = {loginHandler,signupHandler};
+module.exports = {loginHandler,signupHandler,authVerify};
